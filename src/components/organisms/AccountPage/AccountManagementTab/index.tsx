d9fda3e34e41aaae1999bd94e6f4f24d69e9037d
Hudson Santos <work.of.hudson@gmail.com>
2024-10-05 11:46:31 -0300

    feat: AccountManagement tab content

--- src/components/organisms/AccountPage/AccountManagementTab/index.tsx
+++ src/components/organisms/AccountPage/AccountManagementTab/index.tsx
@@ -1,14 +1,43 @@
+import ButtonFullWidth from '@/components/atoms/ButtonFullWidth';
 import {
-    TabContainer,
-    TitleTab
+  Divider,
+  SubtitleTab,
+  TabContainer,
+  TitleTab,
 } from '@/components/organisms/AccountPage/styles';
-  
+import {
+  AccountManagementContent,
+  ExplanationContainer,
+  ExplanationWrapper,
+  ButtonsWrapper,
+} from './styles';
 
 export function AccountManagementTab() {
   return (
     <TabContainer value="account-management">
-    <TitleTab>Gestão da conta</TitleTab>
-    </TabContainer>
+      <TitleTab>Gestão da conta</TitleTab>
+      <SubtitleTab>
+        Você tem duas opções para se ausentar da plataforma: desativar ou
+        excluir sua conta.
+      </SubtitleTab>
 
-  )
+      <AccountManagementContent>
+        <ExplanationWrapper>
+          <ExplanationContainer>
+            Desative sua conta e ela será temporariamente suspensa, mantendo
+            seus dados e permitindo reativação a qualquer momento.
+          </ExplanationContainer>
+          <ExplanationContainer>
+            Ao excluir, ela será deletada permanentemente após 30 dias. Após
+            esse prazo, seus dados serão removidos definitivamente.
+          </ExplanationContainer>
+        </ExplanationWrapper>
+        <Divider />
+        <ButtonsWrapper>
+          <ButtonFullWidth textcolor="gray">Desativar conta</ButtonFullWidth>
+          <ButtonFullWidth textcolor="red">Excluir conta</ButtonFullWidth>
+        </ButtonsWrapper>
+      </AccountManagementContent>
+    </TabContainer>
+  );
 }
---
+++ src/components/organisms/AccountPage/AccountManagementTab/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const AccountManagementContent = styled.section`
+  max-width: 37rem;
+`;
+
+export const ExplanationWrapper = styled.div`
+  margin-top: 1rem;
+  margin-bottom: 1.8rem;
+`;
+
+export const ExplanationContainer = styled.p`
+  color: ${props => props.theme.colors.gray[700]};
+  opacity: 0.9;
+  margin-bottom: 0.7rem;
+  font-size: 0.97rem;
+`;
+
+export const ButtonsWrapper = styled.div`
+  margin-top: 2rem;
+`
